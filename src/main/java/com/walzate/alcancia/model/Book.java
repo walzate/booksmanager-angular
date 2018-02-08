abefe0373ee35400b0725c8f1c7e6b3191b3c1a3
Wilson Alzate Calderon <wilson.alzate@gmail.com>
2018-02-07 21:27:58 -0500

    Added getters and setters, removed aws files

--- src/main/java/com/walzate/alcancia/model/Book.java
+++ src/main/java/com/walzate/alcancia/model/Book.java
@@ -19,4 +19,29 @@ public class Book {
     private Integer id;
     private String isbn;
     private String name;
+
+    public Integer getId(){
+      return id;
+    }
+
+    public void setId(Integer id){
+      this.id = id;
+    }
+
+    public String getIsbn(){
+      return isbn;
+    }
+
+    public void setIsbn(String isbn){
+      this.isbn = isbn;
+    }
+
+    public String getName(){
+      return name;
+    }
+
+    public void setName(String name){
+      this.name = name;
+    }
+
 }
